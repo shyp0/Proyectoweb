8951d76460007a84fde5574b25590695159c3769
shyp0 <vicenteugenio.s@gmail.com>
2023-06-10 18:07:51 -0400

    verificacion lista

--- src/pages/PublicarReceta.jsx
+++ src/pages/PublicarReceta.jsx
@@ -1,48 +1,104 @@
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import '../styles.css';
 
-function PublicarReceta() {
-    return (
-        <div class="container">
-            <h1>Publicar receta</h1>
-            <h4>
-                Puedes publicar la receta que deseas, ¡siempre y cuando sea saludable!
-            </h4>
-            <br></br><br></br>
-            <Form>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Nombre de la receta</Form.Label>
-                    <Form.Control type="text" placeholder="Ingrese nombre de la receta" id="nombre_receta" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Descripción de la receta</Form.Label>
-                    <Form.Control as="textarea" rows={3} placeholder="Ingrese descripción de la receta" id="descripcion_receta" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Ingredientes</Form.Label>
-                    <Form.Control as="textarea" rows={3} placeholder="Ingrese los ingredientes de la receta" id="ingredientes_receta" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Preparación</Form.Label>
-                    <Form.Control as="textarea" rows={3} placeholder="Ingrese la elaboración paso a paso de la receta" id="preparacion:receta" />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label>Nivel de receta</Form.Label>
-                    <Form.Select>
-                        <option>Nivel fácil</option>
-                        <option>Nivel medio</option>
-                        <option>Nivel difícil</option>
+const Formulariorecetas = () => {
+  const [nombre_receta, setNombre_receta] = useState('');
+  const [descripcion_receta, setDescripcion_receta] = useState('');
+  const [ingredientes, setIngredientes] = useState('');
+  const [preparacion, setPreparacion] = useState('');
+  const [files,setFiles] = useState('');
+  const [dificultad,setDificultad] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const validarFormulariorecetas = () => {
+    const errores = {};
+
+    if (!nombre_receta) {
+      errores.nombre_receta = 'El nombre de la receta es obligatorio';
+    }
+
+    if (!descripcion_receta) {
+      errores.descripcion_receta = 'La descripcion de la receta es obligatoria';
+    } else if (descripcion_receta.length > 256) {
+        errores.descripcion_receta = 'La descripcion debe tener menos de 256 caracteres';
+    }
+
+    if (!ingredientes) {
+      errores.ingredientes = 'Los ingredientes son obligatorios';
+    } else if (ingredientes.length > 256) {
+        errores.ingredientes = 'La descripcion de ingredientes debe tener menos de 256 caracteres';
+    }
+
+    if (!preparacion) {
+      errores.preparacion = 'La prepraracion es obligatoria';
+    } else if (preparacion.length > 256) {
+      errores.preparacion = 'La descripcion de la preparacion debe tener menos de 256 caracteres';
+    }
+    if (!dificultad) {
+        errores.dificultad = 'Debes seleccionar al menos un nivel de receta';
+    }
+
+    if (!files) {
+        errores.files = 'Debes registrar almenos 1 archivo';
+    } 
+    setErrors(errores);
+
+    if (Object.keys(errores).length === 0) {
+      // Realizar la lógica de envío del formulario
+      console.log('Formulario válido');
+    }
+  };
+
+  return (
+    <div class="container">
+        <h1>Publicar receta</h1>
+        <h4>
+            Puedes publicar la receta que deseas, ¡siempre y cuando sea saludable!
+        </h4>
+        <br></br><br></br>
+        <Form id='publicarreceta' name='publicarreceta'>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Nombre de la receta</Form.Label>
+                <Form.Control type="text" className={errors.nombre_receta ? 'error' : 'success'} placeholder="Ingrese nombre de la receta" id="nombre_receta" onChange={(e)=> setNombre_receta(e.target.value)} />
+                {errors.nombre_receta && <span>{errors.nombre_receta}</span>}
+            </Form.Group>
+            <Form.Group className="mb-3"  controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Descripción de la receta</Form.Label>
+                <Form.Control as="textarea" className={errors.descripcion_receta ? 'error' : 'success'} rows={3} placeholder="Ingrese descripción de la receta" id="descripcion_receta" onChange={(e)=> setDescripcion_receta(e.target.value)}/>
+                {errors.descripcion_receta && <span>{errors.descripcion_receta}</span>}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Ingredientes</Form.Label>
+                <Form.Control as="textarea" className={errors.ingredientes ? 'error' : 'success'} rows={3} placeholder="Ingrese los ingredientes de la receta" id="ingredientes_receta" onChange={(e)=> setIngredientes(e.target.value)}/>
+                {errors.ingredientes && <span>{errors.ingredientes}</span>}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Preparación</Form.Label>
+                <Form.Control as="textarea" className={errors.preparacion ? 'error' : 'success'} rows={3} placeholder="Ingrese la elaboración paso a paso de la receta" id="preparacion:receta" onChange={(e)=> setPreparacion(e.target.value)}/>
+                {errors.preparacion && <span>{errors.preparacion}</span>}
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Nivel de receta</Form.Label>                
+                    <Form.Select value={dificultad} className={errors.dificultad ? 'error' : ''} onChange={(e)=> setDificultad(e.target.value)}>
+                        <option value=""> seleccione nivel de receta</option>
+                        <option value="facil">Nivel fácil</option>
+                        <option value="medio">Nivel medio</option>
+                        <option value="dificil">Nivel difícil</option>
                     </Form.Select>
-                </Form.Group>
-                <Form.Group controlId="formFileMultiple" className="mb-3">
-                    <Form.Label>Selecione una(s) foto(s) de la receta</Form.Label>
-                    <Form.Control type="file" multiple />
-                </Form.Group>
-                <Button variant="success">Publicar receta</Button>{' '}
-            </Form>
+                    {errors.dificultad && <span>{errors.dificultad}</span>}
+            </Form.Group>
+            <Form.Group controlId="formFileMultiple" className="mb-3">
+                <Form.Label>Selecione una(s) foto(s) de la receta</Form.Label>
+                <Form.Control type="file" className={errors.files ? 'error' : ''} multiple onChange={(e)=> setFiles(e.target.value)}/>
+                {errors.files && <span>{errors.files}</span>}
+            </Form.Group>
+            <Button variant="success" onClick={validarFormulariorecetas}>Publicar receta</Button>{' '}
+        </Form>
 
-        </div>
-    );
-}
+    </div>
+);
+};
 
-export default PublicarReceta;
+export default Formulariorecetas;
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,38 +1,87 @@
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import '../styles.css';
 
-function Registro() {
-    return (
-        <div class="container-registrar">
-            <div class="formulario2">
-                <Form name="registro" id="registro">
-                    <h5>Crear cuenta</h5>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label></Form.Label>
-                        <Form.Control class="controls" type="text" placeholder="Nombre" id="nombre" name="nombre"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label></Form.Label>
-                        <Form.Control class="controls" type="text" placeholder="Apellido" id="apellido" name="apellido"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label></Form.Label>
-                        <Form.Control class="controls" type="email" placeholder="Correo electrónico" id="correo" name="correo"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label></Form.Label>
-                        <Form.Control class="controls" type="password" placeholder="Contraseña" id="contrasena" name="contrasena"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label></Form.Label>
-                        <Form.Control class="controls" type="password" placeholder="Repetir contraseña" id="recontrasena" name="recontrasena"/>
-                    </Form.Group>
-                    <Button class="registrar">Registrar</Button>{' '}
-                </Form>
-            </div>
-        </div>
-    );
-}
+const Formulario = () => {
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [email, setEmail] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  const [confirmarContrasena,setConfirmarContrasena] = useState('');
+  const [errors, setErrors] = useState({});
 
-export default Registro;
+  const validarFormulario = () => {
+    const errores = {};
+
+    if (!nombre) {
+      errores.nombre = 'El nombre es obligatorio';
+    }
+
+    if (!apellido) {
+      errores.apellido = 'El apellido es obligatorio';
+    }
+
+    if (!email) {
+      errores.email = 'El email es obligatorio';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errores.email = 'El email no es válido';
+    }
+
+    if (!contrasena) {
+      errores.contrasena = 'La contraseña es obligatoria';
+    } else if (contrasena.length < 6) {
+      errores.contrasena = 'La contraseña debe tener al menos 6 caracteres';
+    }
+    if (!confirmarContrasena) {
+        errores.confirmarContrasena = 'Debes confirmar la contraseña';
+      } else if (confirmarContrasena !== contrasena) {
+        errores.confirmarContrasena = 'Las contraseñas no coinciden';
+    }
+
+    setErrors(errores);
+
+    if (Object.keys(errores).length === 0) {
+      // Realizar la lógica de envío del formulario
+      console.log('Formulario válido');
+    }
+  };
+
+  return (
+    <div class="container-registrar">
+        <div class="formulario2">
+            <Form name="registro" id="registro">
+                <h5>Crear cuenta</h5>
+                <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+                    <Form.Label></Form.Label>
+                    <Form.Control class="controls" className={errors.nombre ? 'controls error' : 'controls success'} type="text" placeholder="Nombre" id="nombre" name="nombre" onChange={(e)=> setNombre(e.target.value)}/>
+                    {errors.nombre && <span>{errors.nombre}</span>}
+                </Form.Group>
+                <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+                    <Form.Label></Form.Label>
+                    <Form.Control class="controls" className={errors.apellido ? 'controls error' : 'controls success'} type="text" placeholder="Apellido" id="apellido" name="apellido" onChange={(e)=> setApellido(e.target.value)}/>
+                    {errors.apellido && <span>{errors.apellido}</span>}
+                </Form.Group>
+                <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+                    <Form.Label></Form.Label>
+                    <Form.Control class="controls" type="email" className={errors.email ? 'controls error' : 'controls success'} placeholder="Correo electrónico" id="correo" name="correo" onChange={(e)=> setEmail(e.target.value)}/>
+                    {errors.email && <span>{errors.email}</span>}
+                </Form.Group>
+                <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+                    <Form.Label></Form.Label>
+                    <Form.Control class="controls" className={errors.contrasena ? 'controls error' : 'controls success'} type="password" placeholder="Contraseña" id="contrasena" name="contrasena" onChange={(e)=>setContrasena(e.target.value)}/>
+                    {errors.contrasena && <span>{errors.contrasena}</span>}
+                </Form.Group>
+                <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+                    <Form.Label></Form.Label>
+                    <Form.Control class="controls" className={errors.confirmarContrasena ? 'controls error' : 'controls success'} type="password" placeholder="Repetir contraseña" id="recontrasena" name="recontrasena" onChange={(e)=>setConfirmarContrasena(e.target.value)}/>
+                    {errors.confirmarContrasena && <span>{errors.confirmarContrasena}</span>}
+                </Form.Group>
+                <Button class="registrar" onClick={validarFormulario} >Registrar</Button>{' '}
+            </Form>
+        </div>
+    </div>
+);
+};
+
+export default Formulario;
