f1106df0990b4eef95b6f221495c43f7b8ab301b
eyl-lana <eyleen.sanchez.g@mail.pucv.cl>
2023-06-25 01:17:28 -0400

    conectar el frontend de MostrarRecetas con la base de datos

--- src/pages/MostrarRecetas.jsx
+++ src/pages/MostrarRecetas.jsx
@@ -1,3 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+function MostrarRecetas() {
+  const [recetas, setRecetas] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/obtenerRecetas')  // Reemplaza la URL con la dirección de tu backend
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'success') {
+          setRecetas(data.data); // Almacena los datos de las recetas en el estado del componente
+        }
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <br/><br/>
+        <h1>Recetas saludables</h1>
+      </div>
+
+      {recetas.map(receta => (
+        <div key={receta.id}>
+          <div className="row row-cols-1 row-cols-md-2 g-4" style={{paddingTop: "none", padding: "40px",display: "flex", justifyContent:"center",alignItems:"center",flexDirection:"column"}} >
+            <div className="col">
+              <div className="card">
+                <img src={receta.imagen} className="card-img-top" alt=""></img>
+                <div className="card-body">
+                  <h5 className="card-title" style={{textAlign: "center"}}>{receta.nombre_receta}</h5>
+                  <p className="card-text" style={{textAlign: "center"}}>{receta.descripcion}</p>
+                </div>
+                <div className="d-grid gap-2 d-md-block" style={{textAlign: "center"}}>
+                  <Button variant="success" href={`/detallereceta?id=${receta.id}`}>Ver receta</Button>
+                </div>
+                <br/>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MostrarRecetas;
+
+/*
 import Recetas from '../assets/recetas.json'
 import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom';
@@ -41,89 +91,4 @@ function MostrarRecetas(){
     )
 }
 export default MostrarRecetas;
-
-/* <div class="row row-cols-1 row-cols-md-2 g-4" style="padding: 40px;" style="padding-top: none;">
-                <div class="col">
-                    <div class="card">
-                        <img src="assets/receta1.jpg" class="card-img-top" alt="..."></img>
-                        <div class="card-body">
-                            <h5 class="card-title" style="text-align: center;">{recetas.nombre_receta}</h5>
-                            <p class="card-text" style="text-align: center;">{recetas.descripcion}</p>
-                        </div>
-                    </div>
-                </div>
-    </div>*/
-
-/*                                      {
-                                            recetas.ingredientes && recetas.ingredientes.map(data =>{
-                                                return(
-                                                    <p>
-                                                        {data.nombre_ing}
-                                                    </p>
-                                                )
-                                            })
-                                        }
-
-                                        {
-                                            recetas.preparacion && recetas.preparacion.map(data =>{
-                                                return(
-                                                    <p>
-                                                        {data.pasos}
-                                                    </p>
-                                                )
-                                            })
-                                        }*/
-
-/*          <div class="container">
-                <div class="card-body">
-                    <h1 class="card-title" style={{textAlign: "center"}}>{recetas.nombre_receta}</h1>
-                    <h5 class="card-text" style={{textAlign: "center"}}>{recetas.descripcion}</h5>
-                    <p class="card-text" style={{textAlign: "center"}}>
-                        <h6 class="text-body-secondary">Tiempo: 20 minutos</h6>
-                    </p>
-                    <br/>
-                </div>
-                <div class="card mb-3" style={{maxWidth: "10000px"}}>
-                    <br/>
-                    <div class="row g-0">
-                        <div class="col-md-12">
-                            <img src={recetas.imagen} class="img-fluid rounded-start" alt="...">
-                            <p class="card-text"><small class="text-body-secondary">Valoración: </small></p>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="card-body">
-                                <h5 class="card-title">Ingredientes (para 4 personas)</h5>
-                                <p class="card-text">
-                                    <li>
-                                        {
-                                            recetas.ingredientes && recetas.ingredientes.map(data =>{
-                                                return(
-                                                    <p>
-                                                        {data.nombre_ing}
-                                                    </p>
-                                                )
-                                            })
-                                        }
-                                    </li>
-
-                                    <br/><br/>
-
-                                    <h5>Elaboración</h5>
-                                    <li>
-                                        {
-                                            recetas.preparacion && recetas.preparacion.map(data =>{
-                                                return(
-                                                    <p>
-                                                        {data.pasos}
-                                                    </p>
-                                                )
-                                            })
-                                        }
-                                    </li>
-                                </p>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <br/>
-                </div>*/
+*/
